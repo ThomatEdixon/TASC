61748ec640aa204aa51f1341e120e5c7ba7f13f7
ThomatEdixon <108925868+ThomatEdixon@users.noreply.github.com>
2024-10-15 16:24:28 +0700

    static&final

---
+++ JavaCore/static/src/Main.java
@@ -0,0 +1,27 @@
+import object.Test;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // Truy cập thuộc tính và phương thức static mà không cần tạo đối tượng
+        System.out.println("The start: " + Test.count);
+        Test.displayCount();
+
+        // Tạo các đối tượng mới
+        Test obj1 = new Test();
+        Test obj2 = new Test();
+
+        // Truy cập lại sau khi đã tạo đối tượng
+        System.out.println("After instancing object: " + Test.count);
+        Test.displayCount();
+
+        // Truy cập thuộc tính static final MIN_COUNT mà không cần tạo đối tượng
+
+        // Truy cập đến phương thức displayMinCount mà không cần khởi tạo dối tượng
+        Test.displayMinCount();
+
+        // Khởi tạo đối tượng để truy cập phương thức displayLimitCount;
+        Test test = new Test();
+        test.displayLimitCount();
+    }
+}
---
+++ JavaCore/static/src/object/Test.java
@@ -0,0 +1,32 @@
+package object;
+
+public class Test {
+    // Thuộc tính static
+    public static int count = 0;
+
+    // Thuộc tính final
+    public final int MAX_COUNT= 5;
+
+    public static final int MIN_COUNT = 1;
+
+    // Phương thức static
+    public static void displayCount() {
+        System.out.println("The number of object: " + count);
+    }
+    public void displayLimitCount(){
+       // MAX_COUNT = 10 // lỗi khi cố thay đổi giá trị của biến final
+        System.out.println("Max object: " + MAX_COUNT);
+        System.out.println("Min object: " + MIN_COUNT);
+    }
+    public static void displayMinCount(){
+        // không thể gọi biến MAX_COUNT bởi vì nó không được khai báo là static
+        // System.out.println("Số lượng đối tượng tối đa: " + MAX_COUNT);
+        System.out.println("Min object: " + MIN_COUNT);
+    }
+
+
+    public Test() {
+        // Mỗi lần tạo đối tượng, tăng count
+        count++;
+    }
+}
